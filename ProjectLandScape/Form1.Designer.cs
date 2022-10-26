6cb9deee4483b29a177935495a01e9336e82d5ab
M-Malek <malek.michal.k@gmail.com>
2022-10-26 20:55:12 +0200

    Dodaj pliki projektów.

---
+++ ProjectLandScape/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ProjectLandScape
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dodajNowyObiektToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowyObiektToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.znajdźObiektToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.utwórzOSDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.narzędziaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lokalizacjaObiektuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zarzToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dodajNowyObiektToolStripMenuItem,
+            this.narzędziaToolStripMenuItem,
+            this.pomocToolStripMenuItem,
+            this.pomocToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dodajNowyObiektToolStripMenuItem
+            // 
+            this.dodajNowyObiektToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowyObiektToolStripMenuItem,
+            this.znajdźObiektToolStripMenuItem,
+            this.utwórzOSDToolStripMenuItem});
+            this.dodajNowyObiektToolStripMenuItem.Name = "dodajNowyObiektToolStripMenuItem";
+            this.dodajNowyObiektToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.dodajNowyObiektToolStripMenuItem.Text = "Obiekt";
+            // 
+            // nowyObiektToolStripMenuItem
+            // 
+            this.nowyObiektToolStripMenuItem.Name = "nowyObiektToolStripMenuItem";
+            this.nowyObiektToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.nowyObiektToolStripMenuItem.Text = "Nowy obiekt";
+            // 
+            // znajdźObiektToolStripMenuItem
+            // 
+            this.znajdźObiektToolStripMenuItem.Name = "znajdźObiektToolStripMenuItem";
+            this.znajdźObiektToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.znajdźObiektToolStripMenuItem.Text = "Znajdź obiekt";
+            // 
+            // utwórzOSDToolStripMenuItem
+            // 
+            this.utwórzOSDToolStripMenuItem.Name = "utwórzOSDToolStripMenuItem";
+            this.utwórzOSDToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.utwórzOSDToolStripMenuItem.Text = "Utwórz OSD";
+            // 
+            // narzędziaToolStripMenuItem
+            // 
+            this.narzędziaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lokalizacjaObiektuToolStripMenuItem,
+            this.zarzToolStripMenuItem});
+            this.narzędziaToolStripMenuItem.Name = "narzędziaToolStripMenuItem";
+            this.narzędziaToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.narzędziaToolStripMenuItem.Text = "Narzędzia";
+            // 
+            // lokalizacjaObiektuToolStripMenuItem
+            // 
+            this.lokalizacjaObiektuToolStripMenuItem.Name = "lokalizacjaObiektuToolStripMenuItem";
+            this.lokalizacjaObiektuToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.lokalizacjaObiektuToolStripMenuItem.Text = "Lokalizacja obiektu";
+            // 
+            // zarzToolStripMenuItem
+            // 
+            this.zarzToolStripMenuItem.Name = "zarzToolStripMenuItem";
+            this.zarzToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.zarzToolStripMenuItem.Text = "Zarzadzanie terem";
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.pomocToolStripMenuItem.Text = "Finanse";
+            // 
+            // pomocToolStripMenuItem1
+            // 
+            this.pomocToolStripMenuItem1.Name = "pomocToolStripMenuItem1";
+            this.pomocToolStripMenuItem1.Size = new System.Drawing.Size(57, 20);
+            this.pomocToolStripMenuItem1.Text = "Pomoc";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Project LandScape - 0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dodajNowyObiektToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowyObiektToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem znajdźObiektToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem utwórzOSDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem narzędziaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lokalizacjaObiektuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zarzToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ ProjectLandScape/Login.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ProjectLandScape
+{
+    partial class Login
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelLoginInfo = new System.Windows.Forms.Label();
+            this.tbLoginEmail = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tbLoginPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(3, 34);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(32, 13);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Email";
+            this.labelLogin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(3, 63);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "Password";
+            // 
+            // labelLoginInfo
+            // 
+            this.labelLoginInfo.AutoSize = true;
+            this.labelLoginInfo.Location = new System.Drawing.Point(3, 9);
+            this.labelLoginInfo.Name = "labelLoginInfo";
+            this.labelLoginInfo.Size = new System.Drawing.Size(254, 13);
+            this.labelLoginInfo.TabIndex = 2;
+            this.labelLoginInfo.Text = "To log in please enter your user email and password:";
+            // 
+            // tbLoginEmail
+            // 
+            this.tbLoginEmail.Location = new System.Drawing.Point(57, 34);
+            this.tbLoginEmail.Name = "tbLoginEmail";
+            this.tbLoginEmail.Size = new System.Drawing.Size(181, 20);
+            this.tbLoginEmail.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tbLoginPassword
+            // 
+            this.tbLoginPassword.Location = new System.Drawing.Point(57, 60);
+            this.tbLoginPassword.Name = "tbLoginPassword";
+            this.tbLoginPassword.PasswordChar = '*';
+            this.tbLoginPassword.Size = new System.Drawing.Size(181, 20);
+            this.tbLoginPassword.TabIndex = 5;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(18, 86);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(107, 24);
+            this.buttonLogin.TabIndex = 6;
+            this.buttonLogin.Text = "Log in";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(131, 86);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(107, 24);
+            this.buttonRegister.TabIndex = 7;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.tbLoginPassword);
+            this.Controls.Add(this.tbLoginEmail);
+            this.Controls.Add(this.labelLoginInfo);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Name = "Login";
+            this.Size = new System.Drawing.Size(260, 114);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelLoginInfo;
+        private System.Windows.Forms.TextBox tbLoginEmail;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox tbLoginPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonRegister;
+    }
+}
